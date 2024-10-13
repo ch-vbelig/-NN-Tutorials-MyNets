78034127dc637613d8b9a90415752d4d2057aa0d
Belig Chimitov <belig.ch111@gmail.com>
2024-10-13 16:35:29 +1000

    first commit

---
+++ RNNs/Surname Classification/Rnn.py
@@ -0,0 +1,113 @@
+import torch
+import torch.nn as nn
+from utils import ALL_LETTERS, N_LETTERS
+from utils import load_data, line_to_tensor, random_training_example
+import matplotlib.pyplot as plt
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+class RNNNet(nn.Module):
+    def __init__(self, in_features, n_hidden, out_features):
+        super(RNNNet, self).__init__()
+
+        self.rnn = nn.LSTM(
+            input_size=in_features,
+            hidden_size=n_hidden,
+            num_layers=2
+        )
+
+
+        self.out = nn.Linear(
+            in_features=n_hidden,
+            out_features=out_features
+        )
+
+    def forward(self, x):
+        outs, _ = self.rnn(x, None)
+        # last_output = outs[-1, :, :]
+        out = self.out(outs[-1, :, :])
+        return out
+
+# Data
+category_lines, all_categories = load_data()
+
+# Params
+in_features = N_LETTERS
+n_layers = 64
+out_features = len(all_categories)
+
+# Model
+model = RNNNet(in_features, n_layers, out_features)
+model.to(device)
+
+# Loss & optimizer
+learning_rate = 0.01
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+# input_tensor = line_to_tensor('Smith')
+# output = model(input_tensor)
+
+def category_from_output(output):
+    idx = torch.argmax(output).item()
+    return all_categories[idx]
+
+# print(category_from_output(output))
+
+current_loss = 0.0
+all_losses = []
+plot_steps, print_steps = 1000, 5000
+epochs = 100000
+print(next(model.parameters()).is_cuda)
+# train
+for epoch in range(epochs):
+    category, line, category_tensor, line_tensor = random_training_example(category_lines, all_categories)
+    line_tensor = line_tensor.to(device)
+    category_tensor = category_tensor.to(device)
+
+    # Prediction
+    output = model(line_tensor)
+
+    # Loss
+    loss = criterion(output, category_tensor)
+    current_loss += loss.item()
+
+    # Update weights
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    if (epoch + 1) % plot_steps == 0:
+        all_losses.append(current_loss / plot_steps)
+        current_loss = 0
+
+    if (epoch + 1) % print_steps == 0:
+        guess = category_from_output(output)
+        correct = "CORRECT" if guess == category else f"WRONG: should be {category}"
+
+        print(f"{epoch + 1}/{epochs} | Loss: {loss.item():.4f} {line} | {guess} — {correct}")
+
+
+
+plt.figure()
+plt.plot(all_losses)
+plt.show()
+
+
+def predict(input_line):
+
+    with torch.no_grad():
+        line_tensor = line_to_tensor(input_line).to(device)
+
+        output = model(line_tensor)
+
+        guess = category_from_output(output)
+
+        print(f"I think the name '{input_line}' is {guess}")
+
+while True:
+    sentence = input("Input: ")
+    if sentence == 'q':
+        break
+
+    predict(sentence)
---
+++ RNNs/Surname Classification/utils.py
@@ -0,0 +1,92 @@
+import io
+import os
+import unicodedata
+import string
+import glob
+import torch
+import random
+
+DATA_DIR = 'Surname Classification/data/names/*.txt'
+
+# Alphabet small & capital letters & ".,;"
+# abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.,;
+ALL_LETTERS = string.ascii_letters + '.,;'
+N_LETTERS = len(ALL_LETTERS)
+print(N_LETTERS)
+
+# Convert unicode string to ascii string
+# Hosé -> Hose
+def unicode_to_ascii(s):
+    chars = [c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn' and c in ALL_LETTERS]
+    return "".join(chars)
+
+
+def load_data():
+    category_lines = {}
+    all_categories = []
+
+    def find_files(path):
+        return glob.glob(path)
+
+    # Read a file and split into lines
+    def read_lines(filename):
+        lines_original = io.open(filename, encoding='utf-8').read().strip().split('\n')
+        lines_ascii = [unicode_to_ascii(s) for s in lines_original]
+        return lines_ascii
+
+    for filename in find_files(DATA_DIR):
+        category = os.path.splitext(os.path.basename(filename))[0]
+        category_lines[category] = read_lines(filename)
+
+        all_categories.append(category)
+
+    return category_lines, all_categories
+
+"""
+To represent a single letter, one-hot vector is used of size (1, n_letters)
+To make a word, we join a bunch of one-hot encoded vectors into 2D matrix (line_length, n_letters)
+We add 1 dimension because Pytorch assumes everything is in batches -> (line_length, 1, n_letters)
+"""
+
+# find letter index from all_letters
+def letter_to_index(letter):
+    index = ALL_LETTERS.find(letter)
+    return index
+
+# Turn a line into a (line_length, 1, n_letters) vector
+def line_to_tensor(line):
+    tensor = torch.zeros(len(line), 1, N_LETTERS)
+
+    for i, letter in enumerate(line):
+        letter_index = letter_to_index(letter)
+        tensor[i][0][letter_index] = 1
+
+    return tensor
+
+def random_training_example(category_lines, all_categories):
+
+    def random_choice(a):
+        random_inx = random.randint(0, len(a) - 1)
+        return a[random_inx]
+
+    category = random_choice(all_categories)
+    line = random_choice(category_lines[category])
+
+    category_tensor = torch.tensor([all_categories.index(category)], dtype=torch.long) # [[1], [2] ...]
+    line_tensor = line_to_tensor(line)
+
+    return category, line, category_tensor, line_tensor
+
+if __name__ == '__main__':
+    print(ALL_LETTERS)
+    print(unicode_to_ascii("Hosé"))
+
+    category_lines, all_categories = load_data()
+    print(category_lines["Italian"][:4])
+
+    print(line_to_tensor('Jones').size())
+
+
+
+
+load_data()
